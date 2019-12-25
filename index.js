acb0837186d0f6a95f009af070e7f404660eb46d
Adigunawan Marbun <adigunawan1405@gmail.com>
2019-12-25 10:26:28 +0700

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { auth } = require("./middleware");
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || "development";
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 const CategoriesController = require("./controllers/categories");
 const ArticlesController = require("./controllers/articles");
