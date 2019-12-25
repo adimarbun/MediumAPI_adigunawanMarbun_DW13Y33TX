0f08dffd52307a8f9f8db0fbc2f3195f6490a661
Adigunawan Marbun <adigunawan1405@gmail.com>
2019-12-25 10:01:04 +0700

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("express-group-routes");
 const { auth } = require("./middleware");
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
